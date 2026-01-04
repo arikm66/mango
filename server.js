fe76e4cb4a8e206ded74bbdd2498a74f21242fc2
Arik Mimran <arikm66@yahoo.com>
2026-01-04 21:15:24 +0200

    fix: update wildcard route for Express 5 compatibility

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/dist')));
 
   // Handle any requests that don't match the ones above by sending back the index.html file
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
   });
 }
